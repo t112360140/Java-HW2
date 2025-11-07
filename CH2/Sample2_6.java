99e5602fc7708838cecec1b6f23932f20f3bdfcf
t112360140 <155874754+t112360140@users.noreply.github.com>
2025-11-07 13:34:16 +0800

    Add files via upload

---
+++ CH2/Sample2_6.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Sample2_6 {
+    public static void main(String[] args) throws IOException {
+        System.out.println("請輸入a或b");
+
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+        char letter = str.charAt(0);
+
+        switch (letter) {
+            case 'a':
+                System.out.println("輸入的是a");
+                break;
+            case 'b':
+                System.out.println("輸入的是b");
+                break;
+            default:
+                System.out.println("請輸入a或b");
+                break;
+        }
+    }
+}
